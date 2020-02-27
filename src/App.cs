453a08521b871b10c851b9dd24c7e3e252c8d0a4
Jojo <jonas.uliana@passwd.com.br>
2020-02-27 19:52:12 -0300

    Added default application working directory

--- src/App.cs
+++ src/App.cs
@@ -1,10 +1,18 @@
-﻿namespace Alelo.Console
+﻿using System;
+using System.IO;
+
+namespace Alelo.Console
 {
    internal static class App
     {
         private static void Main()
         {
+            var aleloHome = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("ALELO_HOME"))
+                ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), ".alelo")
+                : Environment.GetEnvironmentVariable("ALELO_HOME");
 
+            if (!Directory.Exists(aleloHome))
+                Directory.CreateDirectory(aleloHome);
         }
     }
 }
